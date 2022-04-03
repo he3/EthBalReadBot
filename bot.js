59871a33ac9a872ce7b77e2486f5fec1266eb2df
Henry Erich III <henrys@gmail.com>
2022-04-03 14:40:13 -0400

    env variables fixed

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 const Web3 = require('web3');
 
 //const keys = require('./keys');
-const aws = require('aws-sdk');
+//const aws = require('aws-sdk');
 
-let s3 = new aws.S3({
-    infura: process.env.infura,
-    discord: process.env.discord
-});
+// let s3 = new aws.S3({
+//     infura: process.env.infura,
+//     discord: process.env.discord
+// });
 
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${s3.infura}`));
+const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${process.env.infura}`));
 
 const { Client, Intents } = require('discord.js');
 
@@ -22,8 +22,8 @@ client.on('ready', () => {
     const cyopFundContractAddress = "0x857f29dd5903b2119771f6dd856e825e63f421ef"
     client.api.applications(client.user.id).commands.post({
         data: {
-            name: 'DAO_balance',
-            description: 'Get the DAO_balance.',
+            name: 'dao_balance',
+            description: 'Get the DAO balance.',
             options: [{
                 type: 3,
                 name: 'addresses',
@@ -45,24 +45,11 @@ client.on('ready', () => {
         }
     });
 
-    client.api.applications(client.user.id).commands.post({
-        data: {
-            name: 'test_me_hard',
-            description: 'Get the glitch_reward.',
-            options: [{
-                type: 3,
-                name: 'addresses',
-                description: 'List of addresses',
-                required: true
-            }]
-        }
-    });
-
     client.ws.on('INTERACTION_CREATE', async (interaction) => {
         const commandId = interaction.data.id;
         const commandName = interaction.data.name;
         
-        if (commandName == 'DAO_balance') {
+        if (commandName == 'dao_balance') {
             let addresses = interaction.data.options[0].value.split(/[\s,]+/).filter(x => web3.utils.isAddress(x)).filter(onlyUnique);
             addresses.push(cyopFundContractAddress)
             let acm = [];
@@ -101,19 +88,7 @@ client.on('ready', () => {
                 }
             });
         }
-
-        if (commandName == 'test_me_hard') {
-            let content = 'pass';
-            client.api.interactions(interaction.id, interaction.token).callback.post({
-                data: {
-                    type: 4,
-                    data: {
-                        content
-                    }
-                }
-            });
-        }
     });
 });
 
-client.login(s3.discord);
+client.login(process.env.discord);
